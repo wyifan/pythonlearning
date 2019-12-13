6e24a1b9cf38f88bccfa263f4d5b756f6f331890
Shaofan Wang <v-shaofw@microsoft.com>
2019-12-13 20:58:24 +0800

    multi insert data

--- readbook.py
+++ readbook.py
@@ -2,6 +2,19 @@ import os
 import hashlib
 import uuid
 import datetime
+import pymysql
+
+sqlbase = "INSERT INTO `BOOKS`(`BookId`, `BookName`, `BookPath`, `Md5`) VALUES (%s,%s,%s,%s)"
+
+def writeToDb(sql,paras):
+    try:
+        conn = pymysql.connect("localhost","root","173170329","books")
+        cursor = conn.cursor()
+        cursor.executemany(sql,paras)
+        conn.commit()
+        conn.close()
+    except:
+        print("Error promed")   
 
 #write data to text
 def writeToTest(path,text):
@@ -11,7 +24,12 @@ def writeToTest(path,text):
 # format sql command
 def formatSql(name,path,md5):
     bookid = uuid.uuid1()
-    return "\nINSERT INTO `BOOKS`(`BookId`, `BookName`, `BookPath`, `Md5`, `CreateDate`) VALUES ('%s','%s','%s','%s',CURRENT_TIMESTAMP());" %(bookid,name,path,md5)
+    return "INSERT INTO `BOOKS`(`BookId`, `BookName`, `BookPath`, `Md5`, `CreateDate`) VALUES ('%s','%s','%s','%s',CURRENT_TIMESTAMP());" %(bookid,name,path,md5)
+
+# format the paras in multi execute sql
+def formatListItem(name,path,md5):
+    id = uuid.uuid1()
+    return "('%s','%s','%s','%s')" %(id,name,path,md5)
 
 # find all files in a folder and subfolder
 def findFiles(path, files):
@@ -44,18 +62,41 @@ def computeMd5(file):
     
     return result
 
+def formatPar(paras):
+    data = "("
+    for para in paras:
+        data+=para
+    
+    print(data)
+    return data +")"
+
 if __name__=='__main__':
     print(datetime.datetime.now())
-    fileitems = findFiles("F:\\books",[])
-    print(datetime.datetime.now())
-    sqlcontent = "use books;"
+    fileitems = findFiles("E:\\test",[])
+    print(datetime.datetime.now())    
+    sqlcontent = ""
     sqlpath = "d:\\sql.txt"
+    exesql = ""
+    sqlparas= "("
     for fi in fileitems:
         tpfn = os.path.split(fi)[1]
         if not tpfn.startswith('.'):
             re = computeMd5(fi)
-            sqlcontent += formatSql(re[1],re[0],re[2])
+            text = formatSql(re[1],re[0],re[2])
+            #writeToDb(text)
+            sqlcontent+= "\n" + text
+            exesql += text
+            if sqlparas is "(":
+                sqlparas+= formatListItem(re[1],re[0],re[2])
+            else:
+                sqlparas += (","+formatListItem(re[1],re[0],re[2]))
         #print(fi)
+    sqlparas +=")"
     print(datetime.datetime.now())
+    print(sqlparas)
+    # writeToTest("d:\\par.txt",sqlparas    
+    writeToDb(sqlbase,sqlparas)
     #write to the file
     writeToTest(sqlpath, sqlcontent)
+    writeToTest("d:\\t.txt",exesql)
+ 
---
+++ readmysql.py
@@ -0,0 +1,40 @@
+import pymysql
+
+db = pymysql.connect("localhost","root","173170329","books")
+
+cursor = db.cursor()
+
+sql = "select * from books where bookid='f72d1ef0-1b24-11ea-bb88-70c94ee665be'"
+sqlbase = "INSERT INTO `BOOKS`(`BookId`, `BookName`, `BookPath`, `Md5`) VALUES (%s,%s,%s,%s)"
+data =(('c2906c1c-1da7-11ea-adb9-72c94ee665bd','HTTP2 in Action.pdf','E:\/test\/HTTP2 in Action.pdf','268f6fc8938d61e515dcb4bf15ed8352'),('c2aa98f1-1da7-11ea-b5b5-72c94ee665bd','the road less traveled.pdf','E:\/test\/the road less traveled.pdf','989beea96803c86eb47f80c325f278a8'))
+
+def writeToDb(sql,paras):
+    try:
+        conn = pymysql.connect("localhost","root","173170329","books")
+        cursor = conn.cursor()
+        cursor.executemany(sql,paras)
+        conn.commit()
+        conn.close()
+    except:
+        print("Error promed") 
+
+if __name__=='__main__':
+    writeToDb(sqlbase,data)
+
+# try:
+#     # cursor.execute(sql)
+
+#     insql = "INSERT INTO `BOOKS`(`BookId`, `BookName`, `BookPath`, `Md5`, `CreateDate`) VALUES ('3439bce4-1d9e-11ea-bf4e-72c94ee665bd','Android.4.0网络编程详解.pdf','F:\/books\/Andriod\/Android.4.0网络编程详解.pdf','0a4de4aab827bb10e4a803bc509e9d02',CURRENT_TIMESTAMP());INSERT INTO `BOOKS`(`BookId`, `BookName`, `BookPath`, `Md5`, `CreateDate`) VALUES ('345431b4-1d9e-11ea-836d-72c94ee665bd','Android_Application_Development_For_Dummies.pdf','F:\/books\/Andriod\/Android_Application_Development_For_Dummies.pdf','9a28b5be639db847171a595cb344edd0',CURRENT_TIMESTAMP());"
+
+#     # result = cursor.fetchall()
+#     # for row in result:
+#     #     id = row[0]
+#     #     name = row[1]
+#     #     print("id=%s,name=%s" % (id,name))
+
+#     cursor.executemany(sqlbase,data)
+#     db.commit()
+# except:
+#     print("Error: unable to fetch data")
+
+# db.close()
